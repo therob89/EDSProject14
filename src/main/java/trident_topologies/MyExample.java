73765d17c7eb5616e91fd6cd2a9d71038eb557a0
Roberto Palamaro <roby.palamaro@gmail.com>
2015-03-05 12:04:00 +0100

    ok trident

---
+++ src/main/java/trident_topologies/MyExample.java
@@ -0,0 +1,30 @@
+package trident_topologies;
+
+/**
+ * Created by robertopalamaro on 05/03/15.
+ */
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.LocalDRPC;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.StormTopology;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import storm.trident.TridentState;
+import storm.trident.TridentTopology;
+import storm.trident.operation.BaseFunction;
+import storm.trident.operation.TridentCollector;
+import storm.trident.operation.builtin.Count;
+import storm.trident.operation.builtin.FilterNull;
+import storm.trident.operation.builtin.MapGet;
+import storm.trident.operation.builtin.Sum;
+import storm.trident.testing.FixedBatchSpout;
+import storm.trident.testing.MemoryMapState;
+import storm.trident.tuple.TridentTuple;
+
+
+public class MyExample {
+
+    
+}
--- src/main/java/trident_topologies/TridentWordCount.java
+++ src/main/java/trident_topologies/TridentWordCount.java
@@ -39,9 +39,12 @@ public class TridentWordCount {
     }
 
     public static StormTopology buildTopology(LocalDRPC drpc) {
-        FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3, new Values("the cow jumped over the moon"),
-                new Values("the man went to the store and bought some candy"), new Values("four score and seven years ago"),
-                new Values("how many apples can you eat"), new Values("to be or not to be the person"));
+        FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3,
+                new Values("the cow jumped over the moon"),
+                new Values("the man went to the store and bought some candy"),
+                new Values("four score and seven years ago"),
+                new Values("how many apples can you eat"),
+                new Values("to be or not to be the person"));
         spout.setCycle(true);
 
         TridentTopology topology = new TridentTopology();
