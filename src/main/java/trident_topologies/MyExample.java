3a13603057d427f39f1046dd00fde59d359e504d
Roberto Palamaro <roby.palamaro@gmail.com>
2015-03-10 12:37:39 +0100

    batch ok!

--- src/main/java/trident_topologies/MyExample.java
+++ src/main/java/trident_topologies/MyExample.java
@@ -68,7 +68,7 @@ public class MyExample {
                  while ((str = reader.readLine()) != null) {
                      single_line = new ArrayList<Object>();
                      single_line.add(str);
-                     lineFile.add(new Values((single_line)));
+                     lineFile.add(single_line);
                  }
                  reader.close();
                  fileReader.close();
@@ -140,105 +140,14 @@ public class MyExample {
         }
     }
 
-    public static class FixedBatchSpout2 implements IBatchSpout {
-
-        Fields fields;
-        List<Object>[] outputs;
-        int maxBatchSize;
-        HashMap<Long, List<List<Object>>> batches = new HashMap<Long, List<List<Object>>>();
-
-        public FixedBatchSpout2(Fields fields, int maxBatchSize, List<Object>... outputs) {
-            this.fields = fields;
-            this.outputs = outputs;
-            this.maxBatchSize = maxBatchSize;
-        }
-
-        int index = 0;
-        boolean cycle = false;
-
-        public void setCycle(boolean cycle) {
-            this.cycle = cycle;
-        }
-
-        @Override
-        public void open(Map conf, TopologyContext context) {
-            index = 0;
-            System.out.println("*********************************OPEN METHOD::::");
-
-
-        }
-
-        @Override
-        public void emitBatch(long batchId, TridentCollector collector) {
-            List<List<Object>> batch = this.batches.get(batchId);
-            if(batch == null){
-                batch = new ArrayList<List<Object>>();
-                if(index>=outputs.length && cycle) {
-                    index = 0;
-                }
-                for(int i=0; index < outputs.length && i < maxBatchSize; index++, i++) {
-                    batch.add(outputs[index]);
-                }
-                this.batches.put(batchId, batch);
-            }
-            for(List<Object> list : batch){
-                collector.emit(list);
-            }
-        }
-
-        @Override
-        public void ack(long batchId) {
-            this.batches.remove(batchId);
-        }
-
-        @Override
-        public void close() {
-        }
-
-        @Override
-        public Map getComponentConfiguration() {
-            Config conf = new Config();
-            conf.setMaxTaskParallelism(1);
-            return conf;
-        }
-
-        @Override
-        public Fields getOutputFields() {
-            return fields;
-        }
-
-    }
-
 
     public static StormTopology buildTopology(LocalDRPC drpc) {
-        /*
-        FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3,
-                new Values("the cow jumped over the moon"),
-                new Values("the man went to the store and bought some candy"),
-                new Values("four score and seven years ago"),
-                new Values("how many apples can you eat"),
-                new Values("prova prova prova abcd"),
-                new Values("the man went to the store and bought some candy"),
-                new Values("four score and seven years ago"),
-                new Values("how many apples can you eat"),
-                new Values("prova prova prova abcd"));
-        spout.setCycle(false);
 
-        FixedBatchSpout2 spout = new FixedBatchSpout2(new Fields("sentence"), 3,
-                new Values("the cow jumped over the moon"),
-                new Values("the man went to the store and bought some candy"),
-                new Values("four score and seven years ago"),
-                new Values("how many apples can you eat"),
-                new Values("to be or not to be the person"));
-        spout.setCycle(false);*/
         MYFixedBatchSpout spout = new MYFixedBatchSpout(new Fields("sentence"),3);
         TridentTopology topology = new TridentTopology();
         TridentState wordCounts = topology.newStream("spout1", spout)
-                .parallelismHint(16)
-                .each(new Fields("sentence"), new Split(), new Fields("word"))
-                .groupBy(new Fields("word"))
-                .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count"))
-                .parallelismHint(16);
+                .parallelismHint(16).each(new Fields("sentence"), new Split(), new Fields("word")).groupBy(new Fields("word"))
+                .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count")).parallelismHint(16);
 
         /*
         topology.newDRPCStream("words", drpc).each(new Fields("args"), new Split(), new Fields("word"))
